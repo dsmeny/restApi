4033ae2b256dc100e630ad1a8af2c39edb2f2e2b
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2023-07-04 19:22:59 -0400

    sec commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import express from "express";
 import { readFile, writeFile } from "fs/promises";
 import morgan from "morgan";
 import apiRouter from "./api.js";
+import db from "../db.json";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "get request" });
+});
+
+app.get("/posts", (req, res) => {
+  res.status(200).json(db);
+});
 
 const PORT = process.env.PORT || 3000;
 
