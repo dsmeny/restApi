db4f5273d203b19d512b38e02746c9d2f9512ab2
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2023-07-04 20:31:29 -0400

    adding titles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { readFile, writeFile } from "fs/promises";
 import morgan from "morgan";
-import apiRouter from "./api.js";
 import db from "../db.json" assert { type: "json" };
 const app = express();
 
@@ -10,7 +9,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "get request" });
+  res.status(200).json(db);
+});
+
+app.get("/title/:id", (req, res) => {
+  const id = req.params.id;
+  const record = db[`${id}`];
+
+  if (!record) {
+    res.status(400).json({ error: "Bad Request" });
+  }
+
+  res.status(200).json(record);
+});
+
+app.put("/title/:id", (req, res) => {
+  const id = req.params.id;
+  const record = db[`${id}`];
+  const query = req.query.title;
+
+  if (!record) {
+    res.status(400).json({ error: "Bad Request" });
+  }
+
+  record.title = query;
+
+  res.status(200).json({ title: record.title });
 });
 
 app.get("/:id", (req, res) => {
